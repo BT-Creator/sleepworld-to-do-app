cdc97c2644a5e99fcf918b68781c1abd6a45f756
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-10-02 16:56:53 +0200

    Styled the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,19 +38,19 @@ function App() {
       <h1>Sleepworld To-Do list</h1>
     </header>
     <main>
-      <section>
+      <section className='add-task'>
         <h2>Add a task</h2>
-        <p>Add a task here to add it to the lis:</p>
+        <p>Add a task here to add it to the list:</p>
         <input type="text" placeholder="Task name" onChange={(e) => setToDoName(e.target.value)} />
         <input type="submit" value="Add task" onClick={(e) => addTask(e)}/>
       </section>
-      <section>
+      <section className='to-do'>
         <h2>To-Do list</h2>
         <ul>
           {toDos.map((toDo, index) => <ToDoListItem key={index} title={toDo.name} onToggle={() => moveToDoItem(index, toDo.completed)} />)}
         </ul>
       </section>
-      <section>
+      <section className='completed'>
         <h2>Completed tasks</h2>
         <ul>
           {completedToDos.map((toDo, index) => <ToDoListItem key={index} title={toDo.name} checked={toDo.completed} onToggle={() => moveToDoItem(index, toDo.completed) }/>)}
